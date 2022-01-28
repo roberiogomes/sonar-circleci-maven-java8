e9329ef2cf0de13443a34cc54e08750ce1b9cb27
Robério Gomes Patricio <roberiogomes@gmail.com>
2022-01-28 07:58:17 -0300

    Initial files

---
+++ src/main/java/com/example/sonarcirclecimavenjava8/SonarCircleciMavenJava8Application.java
@@ -0,0 +1,13 @@
+package com.example.sonarcirclecimavenjava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarCircleciMavenJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarCircleciMavenJava8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonarcirclecimavenjava8/SonarCircleciMavenJava8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarcirclecimavenjava8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarCircleciMavenJava8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
